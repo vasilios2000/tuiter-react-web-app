cdb7c8f428b1ee8e1c01a10423c5a27a515e1960
vasilios <gjezo.v@northeastern.edu>
2022-10-23 23:41:13 -0400

    assignment 6 fix

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -28,7 +28,7 @@ bi-gear-fill fs-2 position-relative"></i>
           </li>
         </ul>
         <div className="position-relative mb-2">
-          <img src="/images/starship.jpg" className="w-100"/>
+          <img src="/images/spacex-starship.jpg" className="w-100"/>
           <h1 className="position-absolute wd-nudge-up text-white">
             SpaceX Starship</h1>
         </div>
